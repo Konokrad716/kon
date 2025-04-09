c3b96d82ad2f078e1b4e4a9119a926d9c73d5956
Konokrad716 <utebaliev0436@gmail.com>
2025-04-10 00:29:31 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Konokrad716/testmod
+
+go 1.23.2
---
+++ test.go
@@ -0,0 +1,7 @@
+package testmod
+
+import "fmt"
+
+func Greetings(name string) string {
+	return fmt.Sprintf("Hello, %s!\n", name)
+}
