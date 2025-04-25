294dfda38d279af4b95086a150008f4776fda58a
Konokrad716 <utebaliev0436@gmail.com>
2025-04-26 00:11:07 +0300

    Change Hi to allow multilang

--- test.go
+++ test.go
@@ -1,7 +1,22 @@
 package testmod
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Greetings(name string) string {
-	return fmt.Sprintf("Hello, %s! Have a nice day.\n", name)
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hello, %s! Have a nice day.\n", name), nil
+	case "fr":
+		return fmt.Sprintf("Salut, %s! Passe une bonne journée.\n", name), nil
+	case "ru":
+		return fmt.Sprintf("Здарова, %s! Пиздатого тебе дня.\n", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s! Que tengas un buen día.\n", name), nil
+	case "kz":
+		return fmt.Sprintf("Сәлем, %s! Бүгінгі күніңіз жақсы өтсін.\n", name), nil
+	default:
+		return "", fmt.Errorf("unsupported language: %s", lang)
+	}
 }
